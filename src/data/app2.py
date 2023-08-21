90a48a015552ad9cdcbd1352482816cccf1dc0e9
mahmoud-elgarnosy <mh1644@fayoum.edu.eg>
2023-08-21 19:30:25 +0200

    predicting app

--- src/data/app2.py
+++ src/data/app2.py
@@ -1,9 +1,8 @@
 import dash
 from dash import dcc, html, Input, Output
-import plotly.graph_objs as go
-from notebooks import prediction_utils
-from notebooks import simulate_utils
+import plotly.express as px
 from dash_iconify import DashIconify
+from notebooks import prediction_utils, simulate_utils
 
 app = dash.Dash(__name__)
 
@@ -18,49 +17,85 @@ def get_icon(skill: str):
             skill = skill.split('/')[0]
         else:
             skill = skill.split('/')[1]
-
-    return DashIconify(
-        icon=f"devicon:{skill.lower()}")
+    return DashIconify(icon=f"devicon:{skill.lower()}")
 
 
 def get_checklist(category, skills):
-    if category in ['Languages']:
-        return dcc.Checklist(
-            id={"type": "skill-checkbox", "category": category},
-            options=[{'label': [get_icon(skill), skill.replace('(Delphi_C++ Builder)','')], 'value': skill} for skill in skills],
-            value=[],
-            style={'margin': '10px', 'display': 'grid', 'gridTemplateColumns': 'repeat(2, 1fr)'},
-            labelStyle={'fontSize': '14px', 'margin': '5px'}
-        )
-    else:
-        return dcc.Checklist(
-            id={"type": "skill-checkbox", "category": category},
-            options=[{'label': [get_icon(skill), skill.replace('(Delphi_C++ Builder)','')], 'value': skill} for skill in skills],
-            value=[],
-            style={'margin': '10px', 'verticalAlign': 'top'},
-            labelStyle={'fontSize': '14px', 'margin': '5px'}
-        )
+    return dcc.Checklist(
+        id={"type": "skill-checkbox", "category": category},
+        options=[{'label': [get_icon(skill), skill.replace('(Delphi_C++ Builder)', '')], 'value': skill} for skill in skills],
+        value=[],
+        style={'verticalAlign': 'top', 'width': '100%', 'display': 'flex', 'flexWrap': 'wrap', 'margin': 'auto'},
+        labelStyle={'fontSize': '14px', 'margin': '5px'},
+        inline=True
+    )
+
+
+def create_selected_skills_output(selected_skills_dict):
+    output_children = [html.H2("Selected Skills", style={'textAlign': 'center'})]
+
+    for category, skills in selected_skills_dict.items():
+        if skills:
+            skills_text = ", ".join(skills)
+            category_header = html.Strong(f"{category}: ")
+            skills_span = html.Span(skills_text)
+            category_div = html.Div([category_header, skills_span])
+            output_children.append(category_div)
+
+    if not any(selected_skills_dict.values()):
+        output_children.append("No skills selected.")
+
+    return output_children
 
 
 app.layout = html.Div([
     html.H1("Job Category Predictor", style={'textAlign': 'center'}),
 
-    # Scrollable container for checkboxes
+    # Main container for the page
     html.Div(
-        id="checkbox-container",
+        id="main-container",
         children=[
-            html.Div([
-                html.H3(category, style={'textAlign': 'center'}),
-                get_checklist(category, skills),
-            ])
-            for category, skills in categories_skills.items()
-        ],
-        style={'width': '100%', 'display': 'flex', 'flexWrap': 'wrap', 'margin': 'auto'},
-    ),
+            # Control board on the left half
+            html.Div(
+                id="control-board",
+                children=[
+                    html.H2("Select Skills", style={'textAlign': 'center'}),
+                    *[html.Div([
+                        html.Div(html.H3(category, style={'textAlign': 'center'}), className='column-left'),
+                        html.Div(get_checklist(category, skills), className='column-right'),
+                    ], className='row', style={'width': '100%', 'margin': 'auto', 'paddingBottom': '10px', 'borderBottom': '1px solid #ccc'})
+                        for category, skills in categories_skills.items()],
+                ],
+                style={'width': '60%', 'float': 'left'}
+            ),
 
-    # Bar chart section
-    dcc.Graph(id="probability-chart"),
+            # Chart section on the right half
+            html.Div([
+                html.Div(id="selected-skills-output", className='pretty_container'),
+                html.Div(
+                    id="chart-container",
+                    className='pretty_container',
+                    children=[
+                        html.H2('Current job expectations', style={'textAlign': 'center'}),
+                        dcc.Graph(id="probability-chart"),
+                    ],
+                ),
+
+            ], style={'width': '45%', 'float': 'right'}),
+        ],
+        style={'width': '100%', 'display': 'flex', 'overflow': 'hidden'}
+    )
 ])
+
+
+@app.callback(
+    Output("selected-skills-output", "children"),
+    [Input({"type": "skill-checkbox", "category": category}, "value")
+     for category in categories_skills.keys()]
+)
+def update_selected_skills(*selected_skills):
+    selected_skills_dict = {category: skills for category, skills in zip(categories_skills.keys(), selected_skills)}
+    return create_selected_skills_output(selected_skills_dict)
 
 
 @app.callback(
@@ -73,7 +108,6 @@ def update_chart(*selected_skills):
         return {}
 
     selected_skills = [skill for skills in selected_skills for skill in (skills or [])]
-    print(selected_skills)
 
     if selected_skills:
         # Replace this part with your model's prediction logic
@@ -82,21 +116,63 @@ def update_chart(*selected_skills):
         df = prediction_utils.prepare_df(selected_skills, features_names)
         predictions = prediction_utils.predict_roles(model, df, target_names)
 
-        categories, probabilities = predictions.iloc[:10].index, predictions.iloc[:10].values
-
-        # Create a bar chart
-        # Create a customized bar chart
-        fig = go.Figure(data=[go.Bar(y=categories, x=probabilities, orientation='h')],
-                        layout=dict(title='Predicted Developer Jobs', xaxis_title='Probability', yaxis_title='Job'))
-
-        fig.update_layout(
-            margin=dict(l=20, r=20, t=40, b=20),  # Add margins around the chart
-            paper_bgcolor='rgb(243, 243, 243)',  # Set background color
-            plot_bgcolor='rgb(243, 243, 243)',  # Set plot area color
-        )
+        fig = plot_predictions_jobs(predictions, threshold=10)
         return fig
     else:
         return {}
+
+
+def plot_predictions_jobs(predictions, threshold=20):
+    limited_predictions = predictions[:threshold]
+    text = (limited_predictions.values * 100).astype(int)
+    text = [f"{value}%" for value in text]
+
+    fig = px.bar(predictions,
+                 x=limited_predictions.index,
+                 y=limited_predictions.values,
+                 orientation='v',
+                 text=text,
+                 color=limited_predictions.values,
+                 range_color=[0, 1],
+                 color_continuous_scale='Viridis',
+                 )
+
+    def change_labels(x: str):
+        if 'Developer_' in x:
+            x = x.replace('Developer_', '') + ' dev'
+
+        if ' or ' in x:
+            index_to_delete = x.index(' or ')
+            x = x[index_to_delete + 4:]
+
+        if 'Engineer_' in x:
+            x = x.replace('Engineer_', '') + '  Engineer'
+
+        if x.__contains__('back_end') or x.__contains__('full_stack'):
+            x += ' dev'
+        return x
+
+    new_labels = list(map(change_labels, limited_predictions.index))
+    fig.update_xaxes(tickvals=list(range(len(new_labels))),
+                     ticktext=new_labels)
+
+    fig.update_layout(
+        yaxis_range=[0, 1],
+        title_x=0.5, showlegend=False, yaxis_title=None,
+        xaxis_title=None,
+        yaxis=dict(tickfont={'size': 15}),
+        xaxis=dict(tickfont={'size': 20}),
+        title_font_size=20
+    )
+
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+
+    fig.update_xaxes(tickangle=45, tickfont=dict(family='Rockwell', size=10))
+
+    return fig
 
 
 if __name__ == "__main__":
